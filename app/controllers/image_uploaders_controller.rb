077410ac96ff3c4c2a85340e91ac84378bf2477a
samuelvary1 <sam.vary@streeteasy.com>
2014-08-13 16:05:59 -0400

    fixed comments, trying to add images

--- app/controllers/image_uploaders_controller.rb
+++ app/controllers/image_uploaders_controller.rb
@@ -1,74 +1,36 @@
-class ImageUploadersController < ApplicationController
-  before_action :set_image_uploader, only: [:show, :edit, :update, :destroy]
-
-  # GET /image_uploaders
-  # GET /image_uploaders.json
-  def index
-    @image_uploaders = ImageUploader.all
-  end
-
-  # GET /image_uploaders/1
-  # GET /image_uploaders/1.json
-  def show
-  end
-
-  # GET /image_uploaders/new
+class ImagesController < ApplicationController
   def new
-    @image_uploader = ImageUploader.new
+    @image = Image.new(:article_id => params[:article_id])
   end
 
-  # GET /image_uploaders/1/edit
-  def edit
-  end
-
-  # POST /image_uploaders
-  # POST /image_uploaders.json
   def create
-    @image_uploader = ImageUploader.new(image_uploader_params)
-
-    respond_to do |format|
-      if @image_uploader.save
-        format.html { redirect_to @image_uploader, notice: 'Image was successfully created.' }
-        format.json { render :show, status: :created, location: @image_uploader }
-      else
-        format.html { render :new }
-        format.json { render json: @image_uploader.errors, status: :unprocessable_entity }
-      end
+    @image = Image.new(params[:image])
+    if @image.save
+      flash[:notice] = "Successfully created image."
+      redirect_to @image.article
+    else
+      render :action => 'new'
     end
   end
 
-  # PATCH/PUT /image_uploaders/1
-  # PATCH/PUT /image_uploaders/1.json
+  def edit
+    @image = Image.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @image_uploader.update(image_uploader_params)
-        format.html { redirect_to @image_uploader, notice: 'Image was successfully updated.' }
-        format.json { render :show, status: :ok, location: @image_uploader }
-      else
-        format.html { render :edit }
-        format.json { render json: @image_uploader.errors, status: :unprocessable_entity }
-      end
+    @image = Image.find(params[:id])
+    if @image.update_attributes(params[:image])
+      flash[:notice] = "Successfully updated image."
+      redirect_to @image.article
+    else
+      render :action => 'edit'
     end
   end
 
-  # DELETE /image_uploaders/1
-  # DELETE /image_uploaders/1.json
   def destroy
-    @image_uploader.destroy
-    respond_to do |format|
-      format.html { redirect_to image_uploaders_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @image = Image.find(params[:id])
+    @image.destroy
+    flash[:notice] = "Successfully destroyed image."
+    redirect_to @image.article
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_image_uploader
-      @image_uploader = ImageUploader.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def image_uploader_params
-      params.require(:image_uploader).permit(:title, :description, :image)
-    end
 end
--- db/migrate/20140722235638_add_image_to_articles.rb
+++
@@ -1,8 +0,0 @@
-class AddImageToArticles < ActiveRecord::Migration
-  def change
-  	create_table :images do |t|
-    t.string :article
-    t.string :filename
-  end
-  end
-end
